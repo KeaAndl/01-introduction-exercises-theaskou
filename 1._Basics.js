a9a2c07416ae44169df32f9527061f6b8be6c73f
thea <thsk0001@stud.kea.dk>
2025-01-31 11:06:43 +0100

    Basics.js, first assignment

--- 1._Basics.js
+++ 1._Basics.js
@@ -9,6 +9,7 @@ const lastName = "Latif";
 // show in the console
 // My first name is Anders and my last name is Latif
 
+console.log(firstName, lastName);
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -20,5 +21,6 @@ const increment = 1;
 // The result should be 2025
 // You cannot touch the first or the second line
 
+console.log()
 
 // --------------------------------------
